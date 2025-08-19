59ec44e69142f481c6dbf9d24f9e37adab49f60b
rng048 <rixk0524@gmail.com>
2025-08-19 08:08:48 +0000

    1

--- app.py
+++ app.py
@@ -10,8 +10,12 @@ def Index():
 @app.route("/main",methods=["GET","POST"])
 def main():
     #database
-    
     return(render_template("main.html"))
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    q = float(request.form.get("q"))
+    return(render_template("dbs.html",r=(-50.6*q)+90.2))
 
 if __name__ == "__main__":
     app.run() #ensure that you want to run the app on the cloud
